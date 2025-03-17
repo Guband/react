d60d8eb524b7c3f22d047661b372b2228a90d783
Gueye BANDA <banda.gueye@bygecko.onmicrosoft.com>
2025-03-17 12:47:04 +0100

    getting ready for css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 
 import './App.css'
+import Employee from './components/Employee'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [role, setRole] = useState("dev");
   return (
-    <>
-  
+    <div className='App'>
+      <>
+      <input type="text" onChange={(e) => {
+            setRole(e.target.value);
+      }} />
+    <Employee name=" Banda" role="intern"/>
+    <Employee name="Abdou" role={role}/>
+    <Employee name="Aminata"/>
     </>
+    </div>
   )
 }
 
